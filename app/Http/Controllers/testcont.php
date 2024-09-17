6d7a543f9c0aca45e4f0d6f758637a175f5809b2
ginxFromYt <55861477+ginxFromYt@users.noreply.github.com>
2024-09-17 08:09:45 +0800

    commit all to main branch

---
+++ app/Http/Controllers/testcont.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class testcont extends Controller
+{
+    public function test(){
+        return view('test');
+    }
+}
